ca63d80867c2329f3a488e232a7a6900023e317b
SHINING <jack7770126@gmail.com>
2017-11-18 16:34:41 +0800

    [index] add the index page. add some using info.

--- example/OneUI/tab/index.js
+++ example/OneUI/tab/index.js
@@ -3,11 +3,15 @@ const Tab = {
     const dataset = e.currentTarget.dataset;
     const selectedId = dataset.id
     const tabName = dataset.tabName
-    const data = { tabName, selectedId };
+    const currentIndex =dataset.index
+    // const data = { tabName, selectedId };
+    this.setData({
+      [`${tabName}.selected`]: selectedId
+    })
 
-    console.info('[one:tab:change]', data);
+    console.info('[one:tab:change]');
     if (this.handleOneTabChange) {
-      this.handleOneTabChange(data);
+      this.handleOneTabChange(selectedId, currentIndex);
     } else {
       console.warn('页面缺少 handleOneTabChange 回调函数');
     }
--- example/pages/tab/tab.js
+++ example/pages/tab/tab.js
@@ -1,10 +1,11 @@
 // pages/tab/tab.js
 const One = require('../../OneUI/index');
 console.log(One)
-Page(Object.assign({}, One.Table, {
+Page(Object.assign({}, One.Tab, One.Table, {
 
   data: {
     tab1: {
+      tabName: 'tab1',
       list: [
         {
           id: 1,
@@ -36,6 +37,7 @@ Page(Object.assign({}, One.Table, {
       scroll: true
     },
     tab2: {
+      tabName: 'tab2',
       list: [
         {
           id: 1,
@@ -69,6 +71,7 @@ Page(Object.assign({}, One.Table, {
       fontSize: 14
     },
     tab3: {
+      tabName: 'tab3',
       list: [
         {
           id: 1,
@@ -96,23 +99,122 @@ Page(Object.assign({}, One.Table, {
         }
       ],
       selected: '2',
-      color: '#26a2ff',
+      // color: '#26a2ff',
       fixed: true,
       scroll: true
     },
 
+    tabObj: {
+      tableName: 'tabObj',
+      head: [
+        '参数',
+        '说明',
+        '类型',
+        '可选值',
+        '默认',
+      ],
+      bodyKey: ['name', 'desc', 'type', 'choose', 'default'],
+      rowKey: 'id',
+      body: [
+        {
+          id: 1,
+          name: 'tabName',
+          desc: 'tab名,用于确定传入对象名称',
+          type: 'String',
+          choose: '必填',
+          default: '--'
+        },
+        {
+          id: 2,
+          name: 'list',
+          desc: '需要循环的导航数据',
+          type: 'ArrayObject',
+          choose: '必填',
+          default: '--'
+        },
+        {
+          id: 3,
+          name: 'scroll',
+          desc: '开启 x 方向滑动模式',
+          type: 'Boolean',
+          choose: 'true / false',
+          default: 'false'
+        },
+        {
+          id: 4,
+          name: 'fixed',
+          desc: '开启固定定位',
+          type: 'Boolean',
+          choose: 'true / false',
+          default: 'false'
+        },
+        {
+          id: 5,
+          name: 'height',
+          desc: '导航条高度,单位 px',
+          type: 'Number',
+          choose: '--',
+          default: '45'
+        },
+        {
+          id: 6,
+          name: 'fontSize',
+          desc: '字体大小,单位 px',
+          type: 'Number',
+          choose: '--',
+          default: '14'
+        },
+        {
+          id: 7,
+          name: 'color',
+          desc: '字体颜色,CSS 写法',
+          type: 'String',
+          choose: '--',
+          default: '#ccc'
+        },
+        {
+          id: 8,
+          name: 'selected',
+          desc: '当前激活的项',
+          type: 'String',
+          choose: '--',
+          default: '--'
+        },
+      ],
+      scroll: true,
+      border: false,
+    },
+
+    tabEvent: {
+      tableName: 'tabEvent',
+      head: [
+        '事件名',
+        '说明',
+        '参数'
+      ],
+      body: [
+        {
+          id: 1,
+          name: 'handleOneTabChange',
+          desc: '当导航被点击时触发',
+          params: 'selectId, currentIndex'
+        }
+      ],
+      bodyKey: [
+        'name',
+        'desc',
+        'params'
+      ],
+      scroll: true
+    }
+
   },
 
   onLoad() {
-    console.log(this)
   },
 
-  handleOneTabChange(data) {
-    const id = data.selectedId
-    const tabName = data.tabName
-    this.setData({
-      [`${tabName}.selected`]: id
-    })
+  handleOneTabChange(...arg) {
+    console.log(...arg)
   }
 })
 )
