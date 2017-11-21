1ff6629873b3deefcf790a6b3bf35e3d04873ad6
SHINING <jack7770126@gmail.com>
2017-11-21 17:36:38 +0800

    [capsule] perfect the capsule example page. and index page also.

--- example/pages/capsule/capsule.js
+++ example/pages/capsule/capsule.js
@@ -1,66 +1,65 @@
 // pages/capsule/capsule.js
-Page({
+const One = require('../../OneUI/index.js')
+Page(Object.assign({}, One.Table, {
 
-  /**
-   * 页面的初始数据
-   */
   data: {
-  
-  },
+    options1: {
+      leftText: '限购',
+      rightText: '一折'
+    },
+    options2: {
+      scale: '1',
+      color: '#04be02',
+      leftText: '限购',
+      rightText: '一折'
+    },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+    usage: {
+      tableName: 'usage',
+      rowKey: "id",
+      head: [
+        '参数',
+        '说明',
+        '类型',
+        '可选值',
+        '默认',
+      ],
+      bodyKey: ['name', 'desc', 'type', 'choose', 'default'],
+      body: [
+        {
+          id: 1,
+          name: 'scale',
+          desc: '胶囊的放大系数',
+          type: 'Number',
+          choose: '--',
+          default: 0.8
+        },
+        {
+          id: 2,
+          name: 'color',
+          desc: '胶囊颜色',
+          type: 'String',
+          choose: 'CSS 颜色',
+          default: '#999'
+        },
+        {
+          id: 3,
+          name: 'leftText',
+          desc: '左侧文字',
+          type: 'String',
+          choose: '--',
+          default: '--'
+        },
+        {
+          id: 4,
+          name: 'rightText',
+          desc: '右侧文字',
+          type: 'String',
+          choose: '--',
+          default: '--'
+        }
+      ],
+      scroll: true,
+    }
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
-})
+}))
